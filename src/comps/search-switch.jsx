c8085b53509d66433ffa6225941dcac41f57297c
Ryan H <ryanheal89@gmail.com>
2021-09-10 00:56:09 -0700

    switch using url search

--- src/comps/search-switch.jsx
+++ src/comps/search-switch.jsx
@@ -2,17 +2,25 @@ import { PinDropSharp } from '@material-ui/icons';
 import React from 'react';
 
 function SearchSwitch(props) {
-  const children = React.Children.toArray(props.children)
-  console.log("SearchSwitch");
-  //console.log(props);
+  const children = React.Children.toArray(props.children);
+  //console.log("--- SearchSwitch ---");
 
+  var hasValidChild = false;
+  var childIndex = 0;
   for (var i = 0; i < children.length; i++) {
-    console.log(children[i]);
-    console.log(children[i].props.path);
-    if (children[i].props.path === props.path) {
-      return children[i];
+    const childPath = children[i].props.path;
+    //console.log(childPath + " startsWith " + props.path);
+    if (childPath.startsWith(props.path)) {
+      hasValidChild = true;
+      childIndex = i;
     }
   }
-  //return null;
+  //console.log(hasValidChild + " " + childIndex);
+  //console.log(children[childIndex].props.path + " startsWith " + props.path);
+  if (hasValidChild) {
+    return children[childIndex];
+  } else {
+    return null;
+  }
 }
 export default SearchSwitch;
