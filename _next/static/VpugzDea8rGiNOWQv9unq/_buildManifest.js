7bf342576d12d71ce860b21f6dc8c0457a3bd79e
Ryan H. <ryanheal89@gmail.com>
2026-05-03 23:01:05 -0700

    Updates

--- _next/static/VpugzDea8rGiNOWQv9unq/_buildManifest.js
+++ _next/static/VpugzDea8rGiNOWQv9unq/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/89craft.github.io//_next/:path+",
+        "destination": "/89craft.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
