1faaa5b923c82eb2bc177fd08303edad4347e456
Ryan H. <ryanheal89@gmail.com>
2026-05-03 23:12:29 -0700

    Updates

--- _next/static/Ig-1v21pL-qsYT5Mf4y8U/_buildManifest.js
+++ _next/static/Ig-1v21pL-qsYT5Mf4y8U/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/89craft.github.io//_next/:path+",
-        "destination": "/89craft.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
